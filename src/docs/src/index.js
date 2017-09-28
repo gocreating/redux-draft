772929650954495c3312ac41dd7cf7328fab45b8
翁治平 <gocreating@gmail.com>
2017-09-29 05:31:51 +0800

    Rollback create-react-app from v1.0 to v0.9
    create-react-app v1.0 force the import path  to be inside project root,
    which prevents us from importing redux-draft lib into the doc site. A
    quick solution is to use `npm link` to create symbolic link, but
    unfortunately this somehow breaks the build process of redux-draft lib.
    The current proper way is to rollback the version of create-react-app.

--- src/docs/src/index.js
+++ src/docs/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-snapshot';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
