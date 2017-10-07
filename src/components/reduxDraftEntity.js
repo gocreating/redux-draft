2a0ecda21fc6de44d03f1bfc81ac204a93c912b1
翁治平 <gocreating@gmail.com>
2017-10-08 02:17:55 +0800

    Move helper `getData` into hoc `reduxDraftEntity`

--- src/components/reduxDraftEntity.js
+++ src/components/reduxDraftEntity.js
@@ -3,11 +3,25 @@ import PropTypes from 'prop-types';
 
 export default (WrappedComponent) => {
   class reduxDraftEntity extends Component {
+    constructor() {
+      super();
+      this.getData = this._getData.bind(this);
+    }
+
+    _getData() {
+      let { contentState, entityKey } = this.props;
+      let entity = contentState.getEntity(entityKey);
+      let data = entity.getData();
+
+      return data;
+    }
+
     render() {
       let { _reduxDraft } = this.context;
 
       return (
         <WrappedComponent
+          getData={this.getData}
           {..._reduxDraft}
           {...this.props}
         />
