54a919d1f65eabf4cd50bf9b550c251bd49d7129
翁治平 <gocreating@gmail.com>
2017-10-08 15:02:42 +0800

    Upgrade Image component

--- docs/src/draftComponents/Image/Image.js
+++ docs/src/draftComponents/Image/Image.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import noImageAvailable from '../../../public/no_image_available.jpg';
 import './Image.css';
 
-let Image = ({ block, alt, ...rest }) => (
-  <img
-    className="rd image"
-    alt={alt || 'Image Not Found'}
-    {...rest}
-  />
-);
+class Image extends Component {
+  constructor() {
+    super();
+    this.handleImageError = this._handleImageError.bind(this);
+    this.state = {
+      isError: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.src !== this.props.src) {
+      this.setState({ isError: false });
+    }
+  }
+
+  _handleImageError() {
+    this.setState({ isError: true });
+  }
+
+  render() {
+    let { alt, src, ...rest } = this.props;
+    let { isError } = this.state;
+    let cx = classNames('rd image', {
+      'failed': isError,
+    });
+
+    return (
+      <img
+        className={cx}
+        alt={alt || 'No Image Available'}
+        src={isError ? noImageAvailable : src}
+        onError={this.handleImageError}
+        {...rest}
+      />
+    );
+  }
+}
 
 export default Image;
