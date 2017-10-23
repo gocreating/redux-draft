a8a0f7c224918feb8a8b291c7260aacffecb5929
翁治平 <gocreating@gmail.com>
2017-10-23 11:04:11 +0800

    Export action creators

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as reduxDraft } from './components/reduxDraft';
 export { default as reduxDraftEntity } from './components/reduxDraftEntity';
+export * as actionCreators from './actions/index';
 export { default as reducer } from './reducers/index';
